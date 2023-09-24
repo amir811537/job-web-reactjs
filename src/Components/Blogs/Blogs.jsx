c572b8a0c4932db7c0445422caa4436d0337e73f
amir811537 <amirhossain.bc.75@gmail.com>
2023-09-24 15:52:45 +0600

    blogs page added

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,11 +1,38 @@
-
+import React from 'react';
 
 const Blogs = () => {
-    return (
-        <div>
-            <h1>this is blog site</h1>
+  return (
+    <div className="bg-gray-100 min-h-screen py-8">
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-semibold mb-6">Job Site Blog</h1>
+        
+        {/* Blog Post 1 */}
+        <div className="bg-white rounded-lg shadow-md p-4 md:p-6 mb-6">
+          <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold mb-2">How to Find Your Dream Job</h2>
+          <p className="text-gray-600 mb-2 md:mb-4">Published on September 24, 2023 by John Doe</p>
+          <p className="text-gray-800">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non orci vel eros semper feugiat.
+            Vivamus id ante ut ipsum fringilla cursus. Integer venenatis bibendum mi, eget bibendum justo
+            dignissim eu. Sed euismod lectus eu enim tincidunt, id bibendum libero posuere.
+          </p>
         </div>
-    );
+
+        {/* Blog Post 2 */}
+        <div className="bg-white rounded-lg shadow-md p-4 md:p-6 mb-6">
+          <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold mb-2">Top Tech Companies to Work for in 2023</h2>
+          <p className="text-gray-600 mb-2 md:mb-4">Published on September 20, 2023 by Jane Smith</p>
+          <p className="text-gray-800">
+            In hac habitasse platea dictumst. Praesent tincidunt justo et mauris malesuada, eget vestibulum
+            odio tristique. Nam feugiat eros ut lorem ultrices malesuada. Sed efficitur mauris vel gravida
+            ultricies.
+          </p>
+        </div>
+        
+        {/* Add more blog posts here */}
+        
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,11 +1,45 @@
 
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
 const Statistics = () => {
-    return (
-        <div>
-           <h3> this is statistics path</h3>
+  const jobData = [
+    {
+      "id": 1,
+      "job_title": "Technical Database Engineer",
+      "company_name": "Google LLC",
+      "min_salary": 100000,
+      "max_salary": 150000
+    },
+    {
+      "id": 2,
+      "job_title": "Senior Product Designer",
+      "company_name": "Netflix",
+      "min_salary": 200000,
+      "max_salary": 300000
+    },
+    // ... (other job data entries)
+  ];
+
+  return (
+    <div className='text-center'>
+      <div className='flex justify-center items-center h-screen'>
+        <div className='w-full md:w-3/4 lg:w-1/2'>
+          <BarChart
+            width={window.innerWidth <= 768 ? window.innerWidth - 20 : 600} // Adjust width for mobile screens
+            height={window.innerWidth <= 768 ? 300 : 400} // Adjust height for mobile screens
+            data={jobData}
+          >
+            <XAxis dataKey="company_name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="min_salary" fill="#8884d8" />
+            <Bar dataKey="max_salary" fill="#82ca9d" />
+          </BarChart>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;
